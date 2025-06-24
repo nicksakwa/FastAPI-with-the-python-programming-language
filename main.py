744ba2bb5caf4c3bad19bc931cb03f201464b91e
nsakwa <nsakwa@gmail.com>
2025-06-24 22:18:21 +0300

    updated main.py code

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from fastapi import FASTAPI
-app= FastAPI()
+from fastapi import FastAPI
+
+app = FastAPI()
+
 @app.get("/")
 async def read_root():
-    return {"message": "Hello, FasrAPI"}
-@app.get("item/{item_id}")
-async def read_item(item_id:int, query_param: str= None);
-    return{"item_id":item_id, "query_param": query_param}
+    return {"message": "Hello, FastAPI!"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, query_param: str = None):
+    return {"item_id": item_id, "query_param": query_param}
